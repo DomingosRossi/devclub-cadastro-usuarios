ca8fb05e3fd7165d28502dd507dece86331a05b9
DomingosRossi <domingos_neto@msn.com>
2024-07-31 20:36:43 -0300

    add biblioteca useRef

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,25 @@
+import { useRef } from 'react';
+import api from '../../services/api';
+
 import { Button, Container, Form, Input, InputLabel, Title, TopBackground, ContainerInputs } from './styles'; // ajuste o caminho conforme necessário
 import UsersImgage from '../../assets/users.png';
 
 function Home() {
+  const inputName = useRef()
+  const inputAge = useRef()
+  const inputEmail = useRef()
+
+ async function registerNewUser() {
+    const data = await api.post('/usuarios', {
+      email: inputEmail.current.value,
+      age: parseInt(inputAge.current.value),
+      name: inputName.current.value
+    })
+
+    console.log(data)
+  }
+
+
   return (
     <Container>
       <TopBackground>
@@ -14,22 +32,22 @@ function Home() {
         <ContainerInputs>
             <div>
               <InputLabel>Nome<span> *</span></InputLabel>
-              <Input type='text' placeholder="Nome do usuário" />
+              <Input type='text' placeholder="Nome do usuário" ref={inputName} />
             </div>
 
             <div>
               <InputLabel>Idade<span> *</span></InputLabel>
-              <Input type="number" placeholder="Idade do usuário" />
+              <Input type="number" placeholder="Idade do usuário" ref={inputAge} />
             </div>
           
           </ContainerInputs>
 
           <div style={{ width: '100%'}}>
             <InputLabel>E-mail<span> *</span></InputLabel>
-            <Input type="email" placeholder="E-mail do usuário" />
+            <Input type="email" placeholder="E-mail do usuário" ref={inputEmail} />
           </div>
         
-        <Button>Cadastrar Usuário</Button>
+        <Button type='button' onClick={registerNewUser}>Cadastrar Usuário</Button>
       </Form>
     </Container>
   );
