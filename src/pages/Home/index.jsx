19f8396dfe38b52324b94db0bbe400c2e8f669df
DomingosRossi <domingos_neto@msn.com>
2024-08-05 20:54:13 -0300

    retirado o data do await

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,14 +9,14 @@ function Home() {
   const inputAge = useRef()
   const inputEmail = useRef()
 
- async function registerNewUser() {
-    const data = await api.post('/usuarios', {
+  async function registerNewUser() {
+    await api.post('/usuarios', {
       email: inputEmail.current.value,
       age: parseInt(inputAge.current.value),
       name: inputName.current.value
     })
 
-    console.log(data)
+
   }
 
 
@@ -30,27 +30,27 @@ function Home() {
         <Title>Cadastrar Usuário</Title>
 
         <ContainerInputs>
-            <div>
-              <InputLabel>Nome<span> *</span></InputLabel>
-              <Input type='text' placeholder="Nome do usuário" ref={inputName} />
-            </div>
-
-            <div>
-              <InputLabel>Idade<span> *</span></InputLabel>
-              <Input type="number" placeholder="Idade do usuário" ref={inputAge} />
-            </div>
-          
-          </ContainerInputs>
-
-          <div style={{ width: '100%'}}>
-            <InputLabel>E-mail<span> *</span></InputLabel>
-            <Input type="email" placeholder="E-mail do usuário" ref={inputEmail} />
+          <div>
+            <InputLabel>Nome<span> *</span></InputLabel>
+            <Input type='text' placeholder="Nome do usuário" ref={inputName} />
           </div>
-        
+
+          <div>
+            <InputLabel>Idade<span> *</span></InputLabel>
+            <Input type="number" placeholder="Idade do usuário" ref={inputAge} />
+          </div>
+
+        </ContainerInputs>
+
+        <div style={{ width: '100%' }}>
+          <InputLabel>E-mail<span> *</span></InputLabel>
+          <Input type="email" placeholder="E-mail do usuário" ref={inputEmail} />
+        </div>
+
         <Button type='button' onClick={registerNewUser}>Cadastrar Usuário</Button>
       </Form>
     </Container>
   );
 }
 
-export default Home;
+export default Home;
